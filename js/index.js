7d5c285d24b72432f90da3897f232627e57c7553
z1mbura <106754327+z1mbura@users.noreply.github.com>
2022-11-22 09:57:46 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ navBurger.addEventListener('click', () => {
 const galleryButton = document.querySelector('.gallery-but')
 const galleryItem = document.querySelector('.gallery-item')
 galleryButton.addEventListener('mouseover', () => {
-    if(galleryItem.classList != 'shadow') {
+    if(galleryItem.classList.contains('shadow')) {
         galleryItem.classList.add('shadow')
         }else {
             galleryItem.classList.remove('shadow')    
