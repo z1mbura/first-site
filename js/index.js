b1035ed529a88c5a531948dea84d49b592e7ee9c
z1mbura <106754327+z1mbura@users.noreply.github.com>
2022-11-22 13:02:09 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,11 +4,11 @@ const galleryItem = document.querySelectorAll('.gallery-item')
 galleryButton.forEach(function(item) {
     item.addEventListener('mouseover', (event) => {
         if(galleryItem.classList != 'shadow') {
-            galleryItem[event.target].classList.add('shadow')
+            galleryItem[int(item)].classList.add('shadow')
         }
     })
     item.addEventListener('mouseout', (event) => {
-        galleryItem[event.target].classList.remove('shadow')
+        galleryItem[int(item)].classList.remove('shadow')
     })
 })
 
