fdf5ceb8894ecc9efc5a7d3ead7a237fea08cf8e
z1mbura <106754327+z1mbura@users.noreply.github.com>
2022-11-22 12:09:29 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const navBurger = document.querySelector('.con')
 const navBlock = document.querySelector('.header-nav')
-
+const galleryButton = document.querySelector('.gallery-but')
+const galleryItem = document.querySelector('.gallery-item')
 navBurger.addEventListener('click', () => {
     if(navBurger.classList != 'active'){
         navBurger.classList.toggle('active')
@@ -9,12 +10,7 @@ navBurger.addEventListener('click', () => {
 
 })
 
-
-const galleryButton = document.querySelector('.gallery-but')
-const galleryItem = document.querySelector('.gallery-item')
-
-
-galleryButton.addEventListener('mouseout', () => {
+galleryButton.addEventListener('mouseover', () => {
     console.log("True")
-    })
+})
 
