a738280acc7cc0fe4863594a8028a8bc720bbcdd
z1mbura <106754327+z1mbura@users.noreply.github.com>
2022-11-22 12:39:36 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const galleryButton = document.querySelectorAll('.gallery-but')
 const galleryItem = document.querySelectorAll('.gallery-item')
 
-galleryButton.forEach(function(i, element) {
-    element.addEventListener('mouseover', () => {
+galleryButton.forEach(function(i, item) {
+    item.addEventListener('mouseover', () => {
         if(galleryItem.classList != 'shadow') {
             console.log('hui')
             galleryItem[i].classList.add('shadow')
